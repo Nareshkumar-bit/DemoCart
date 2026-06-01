0c0ced13208eeed07dd7c87f24664add9e8723dc
Nareshkumar-bit <nareshmurali2424@gmail.com>
2026-06-01 22:26:17 +0530

    first commit

---
+++ src/main/java/DemoCart/DemoCart/App.java
@@ -0,0 +1,13 @@
+package DemoCart.DemoCart;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
